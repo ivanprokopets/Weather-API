05d234aaf2a78e648d5aaab5854746270cd45fd0
ivanprokopets <ivanprokopets1999@gmail.com>
2019-12-05 02:56:03 +0100

    Version 1.0 for checking weather

---
+++ script.js
@@ -0,0 +1,31 @@
+$("#city-form").submit(function(event) {
+		event.preventDefault();
+
+		var cityName = $("#city-name").val();
+		getWeather(cityName);
+	});
+
+	function getWeather(city){
+		$.get("http://api.openweathermap.org/data/2.5/weather",
+			{
+				"q":city,
+				"lang":"pl",
+				"appid":"79f4c706e21b976faa3aee2a3a75e6c4"
+			},
+			function(data){
+				let out = '';
+				out += 'Kraj: <b>' +data.sys.country +'</b><br>';
+				out += 'Miasto: <b>' + data.name + '</b><br>';
+				out += 'Pogoda: <b>'+ data.weather[0].main+'</b><br>';
+				out += 'Szczegolu Pogoda: <b>'+ data.weather[0].description+'</b><br>';
+				out += '<img src="https://openweathermap.org/img/w/'+data.weather[0].icon+'.png"><br>';
+				out += 'Temperatura: <b>' +Math.round(data.main.temp-273)+'&#176;C</b><br>';
+				out += 'Wilgotnosc: <b>' +data.main.humidity +'%</b><br>';
+				out += 'Cisnienie: <b>' + data.main.pressure +' hPa</b><br>';
+				out += 'Widocznosc: <b>' + (data.visibility/1000) + 'km</b><br>';
+				out += 'Wiatr: <b>' + data.wind.speed + 'm/s</b><br>';
+				out += 'Zachmurzenie: <b>' + data.clouds.all + '%</b><br>';
+				console.log(data);
+			$('#weather').html(out);
+		});
+	}
